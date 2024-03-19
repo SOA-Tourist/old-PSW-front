9311c9ac0e0bf78a4423d823ca149088901453f1
Pisaric <116560852+Pisaric@users.noreply.github.com>
2024-03-19 10:48:37 +0100

    tourist encounter

--- Explorer/src/app/feature-modules/encounters/encounter-draw.service.ts
+++ Explorer/src/app/feature-modules/encounters/encounter-draw.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Encounter, EncounterStatistics, EncounterType } from './model/encounters.model';
+import {
+  Encounter,
+  EncounterStatistics,
+  EncounterType,
+} from './model/encounters.model';
 import { EncounterExecutionService } from './encounter-execution.service';
 import { EncounterService } from './encounter.service';
 import { EMPTY, Observable } from 'rxjs';
@@ -8,13 +12,19 @@ import { ToastrService } from 'ngx-toastr';
 import { ImageService } from 'src/app/shared/image.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EncounterDrawService {
+  constructor(
+    private imageService: ImageService,
+    private executionService: EncounterExecutionService,
+    private toastr: ToastrService
+  ) {}
 
-  constructor(private imageService: ImageService, private executionService: EncounterExecutionService, private toastr: ToastrService) { }
-
-  generateEncounterPopUp(encounter: Encounter, encounterStats: EncounterStatistics): string {
+  generateEncounterPopUp(
+    encounter: Encounter,
+    encounterStats: EncounterStatistics
+  ): string {
     let specifics = '';
     console.log(encounter);
     if (encounter.type.toString() === 'Social') {
@@ -38,40 +48,54 @@ export class EncounterDrawService {
     //     specifics = this.generateMiscEncounterSpecifics(encounter);
     //     break;
     // }
-    return this.generateCommonEncounterHTML(encounter, encounterStats) + specifics + this.generateEncounterButton(encounter.id!);
+    return (
+      this.generateCommonEncounterHTML(encounter, encounterStats) +
+      specifics +
+      this.generateEncounterButton(encounter.id!)
+    );
   }
 
-  generateCommonEncounterHTML(encounter: Encounter, encounterStats: EncounterStatistics): string {
+  generateCommonEncounterHTML(
+    encounter: Encounter,
+    encounterStats: EncounterStatistics
+  ): string {
     return `<div style="text-align: center; padding: 10px; padding-bottom: 0px; font-size: 1.15em;">
-              <h2 style="font-weight: bold; margin-bottom: 10px;">${encounter.name}</h2>
+              <h2 style="font-weight: bold; margin-bottom: 10px;">${
+                encounter.name
+              }</h2>
               <p style="margin-bottom: 10px;">${encounter.description}</p>
-              <p style="margin-bottom: 10px;"><strong>Type:</strong> ${EncounterType[encounter.type]}</p>
-              <p style="margin-bottom: 10px;"><strong>XP:</strong> ${encounter.xp}</p>
-              <p style="margin-bottom: 10px;"><strong>Success Rate:</strong> ${encounterStats.completedCount} / ${encounterStats.completedCount + encounterStats.abandonedCount} completed</p>
+              <p style="margin-bottom: 10px;"><strong>Type:</strong> ${
+                EncounterType[encounter.type]
+              }</p>
+              <p style="margin-bottom: 10px;"><strong>XP:</strong> ${
+                encounter.xp
+              }</p>
+              <p style="margin-bottom: 10px;"><strong>Success Rate:</strong> ${
+                encounterStats.completedCount
+              } / ${
+      encounterStats.completedCount + encounterStats.abandonedCount
+    } completed</p>
             </div>`;
   }
-  
+
   generateSocialEncounterSpecifics(encounter: Encounter): string {
     return `<div style="text-align: center; font-size: 1.15em;"><strong>Required People:</strong> ${encounter.socialEncounterRequiredPeople}</div><br>`;
   }
-  
+
   generateHiddenLocationEncounterSpecifics(encounter: Encounter): string {
     const imageUrl = this.getFullImageUrl(encounter.imageUrl);
     return `<div style="display: flex; justify-content: center; font-size: 1.15em; margin-bottom: 20px;">
               <img src="${imageUrl}" alt="Image" style="max-height: 175px; width: 270px;">
             </div>`;
   }
-  
+
   generateMiscEncounterSpecifics(encounter: Encounter): string {
     return `<div style="text-align: center; font-size: 1.15em;"><strong>Task:</strong> ${encounter.miscEncounterTask}</div><br>`;
   }
-  
-  activateEncounter(encounterId: number): void{
-    alert("micasigurica")
-  }
+
+  activateEncounter(encounterId: number): void {}
 
   generateEncounterButton(encounterId: any): string {
-    alert(encounterId)
     return `<div style="display: flex; justify-content: center;">
               <button onclick="activateEncounter('${encounterId}')" style="background-color: #315149; border: none; border-radius: 20px; padding: 10px 20px; font-size: 1.2em; color: white; transition: all 0.3s ease 0s; cursor: pointer; outline: none;">
                 Activate Encounter
@@ -79,10 +103,8 @@ export class EncounterDrawService {
             </div>`;
   }
   getFullImageUrl(imageURL?: string): string {
-    if (imageURL)
-      return this.imageService.getFullImageUrl(imageURL);
-    else
-      return "No Image";
+    if (imageURL) return this.imageService.getFullImageUrl(imageURL);
+    else return 'No Image';
   }
 
   showEncounterCreated(): void {
@@ -98,19 +120,31 @@ export class EncounterDrawService {
   }
 
   showHiddenLocationFound(): void {
-    this.toastr.success('You\' found the hidden location, stay put for 30 seconds to complete the encounter.', 'Hidden location found');
+    this.toastr.success(
+      "You' found the hidden location, stay put for 30 seconds to complete the encounter.",
+      'Hidden location found'
+    );
   }
 
   showHiddenLocationExited(): void {
-    this.toastr.warning('Yo\'ve gone too far from the hidden location.', 'Hidden location exited');
+    this.toastr.warning(
+      "Yo've gone too far from the hidden location.",
+      'Hidden location exited'
+    );
   }
 
   showEncounterActivated(): void {
-    this.toastr.info('You activated the encounter successfully.', 'Encounter activated');
+    this.toastr.info(
+      'You activated the encounter successfully.',
+      'Encounter activated'
+    );
   }
 
   showEncounterCompleted(): void {
-    this.toastr.success('You finished the encounter successfully.', 'Encounter completed');
+    this.toastr.success(
+      'You finished the encounter successfully.',
+      'Encounter completed'
+    );
   }
 
   showUnableToActivate(message: string): void {
@@ -118,15 +152,23 @@ export class EncounterDrawService {
   }
 
   showAlreadyCompleted(): void {
-    this.toastr.warning('You already completed the encounter..', 'Encounter can not be activated');
+    this.toastr.warning(
+      'You already completed the encounter..',
+      'Encounter can not be activated'
+    );
   }
 
   showEncounterAbandoned(): void {
-    this.toastr.success('You abandoned the active encounter.', 'Encounter abandoned');
+    this.toastr.success(
+      'You abandoned the active encounter.',
+      'Encounter abandoned'
+    );
   }
 
   showForcedEncounterAbandon(): void {
-    this.toastr.warning('Encounter abandoned, you\'ve left the encounter range.', 'Encounter abandoned');
+    this.toastr.warning(
+      "Encounter abandoned, you've left the encounter range.",
+      'Encounter abandoned'
+    );
   }
-
 }
--- Explorer/src/app/feature-modules/encounters/encounter-execution.service.ts
+++ Explorer/src/app/feature-modules/encounters/encounter-execution.service.ts
@@ -3,40 +3,69 @@ import { Injectable } from '@angular/core';
 import { EncounterExecution } from './model/encounter-execution.model';
 import { Coordinate } from '../tour-execution/model/coordinate';
 import { environment } from 'src/env/environment';
-import { PagedResults } from "src/app/shared/model/paged-results.model";
+import { PagedResults } from 'src/app/shared/model/paged-results.model';
 import { Observable } from 'rxjs';
 import { EncounterStatistics } from './model/encounters.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EncounterExecutionService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  activate(encounterId: number, coordinate: Coordinate): Observable<EncounterExecution> {
+  // Promenio
+  activate(
+    encounterId: number,
+    coordinate: Coordinate
+  ): Observable<EncounterExecution> {
     return this.http.post<EncounterExecution>(
-      environment.apiHost + 'tourist/execution/encounter/' + encounterId, coordinate);
+      environment.apiHost + 'tourist/execution/encounter/' + encounterId,
+      coordinate
+    );
   }
 
-  abandon(executionId: number): Observable<EncounterExecution> {
+  //Promenio
+  abandon(executionId: any): Observable<EncounterExecution> {
     return this.http.patch<EncounterExecution>(
-      environment.apiHost + 'tourist/execution/encounter/abandon', executionId);
+      environment.apiHost +
+        'tourist/execution/encounter/abandon/' +
+        executionId,
+      { executionId: executionId }
+    );
+  }
+  // abandon(executionId: any): Observable<EncounterExecution> {
+  //   return this.http.patch<EncounterExecution>(
+  //     environment.apiHost + 'tourist/execution/encounter/abandon',
+  //     { "executionId": executionId }
+  //   );
+  // }
+
+  checkIfCompleted(
+    executionId: number,
+    coordinate: Coordinate
+  ): Observable<EncounterExecution> {
+    return this.http.patch<EncounterExecution>(
+      environment.apiHost + 'tourist/execution/encounter/' + executionId,
+      coordinate
+    );
   }
 
-  checkIfCompleted(executionId: number, coordinate: Coordinate): Observable<EncounterExecution> {
+  //Promenio
+  completeMiscEncounter(executionId: any): Observable<EncounterExecution> {
     return this.http.patch<EncounterExecution>(
-      environment.apiHost + 'tourist/execution/encounter/' + executionId, coordinate);
+      environment.apiHost +
+        'tourist/execution/encounter/completeMiscEncounter/' +
+        executionId,
+      executionId
+    );
   }
 
-  completeMiscEncounter(executionId: number): Observable<EncounterExecution> {
-    return this.http.patch<EncounterExecution>(
-      environment.apiHost + 'tourist/execution/encounter/completeMiscEncounter', executionId);
-  }
-  
-  getEncounterStatsForTourist(touristId: number): Observable<EncounterStatistics> {
+  getEncounterStatsForTourist(
+    touristId: number
+  ): Observable<EncounterStatistics> {
     return this.http.get<EncounterStatistics>(
-        environment.apiHost + `tourist/execution/encounter/statistics/user/${touristId}`);
+      environment.apiHost +
+        `tourist/execution/encounter/statistics/user/${touristId}`
+    );
   }
-
 }
